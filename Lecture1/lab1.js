b9efb95e647b9882e7168886c0fd4e1a77ddbacd
Jon <rfishkin1313@gmail.com>
2018-08-28 16:27:46 -0400

    update

---
+++ Lecture1/lab1.js
@@ -0,0 +1,49 @@
+const questionOne = function questionOne(arr) {
+    let sum = 0
+    arr.forEach(element => {
+        sum = sum + (element * element);
+    });
+    return sum;
+}
+
+const questionTwo = function questionTwo(num) { 
+    if(num <= 1){
+        return num;
+    }
+    return questionTwo(num-1) + questionTwo(num-2);
+}
+
+const questionThree = function questionThree(text) {
+    let sum = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i<=text.length; i++){
+        vowels.forEach(element => {
+            if(text.charAt(i).includes(elements)){
+                console.log('This works');
+            }
+        });
+    }
+    return sum;
+}
+
+const questionFour = function questionFour(num) {
+    let sum = 1;
+    if(num < 0){
+        return NaN;
+    } else {
+        for(let i = 1; i <= num; i++){
+            sum = sum * i;
+        }
+    }
+    return sum;
+}
+
+module.exports = {
+    firstName: "Jonathan", 
+    lastName: "Fishkin", 
+    studentId: "10394200",
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour
+};
