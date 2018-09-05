635bd700c3ebe094362cbd601fe0eb47ed740f74
Thefish1996 <rfishkin1313@gmail.com>
2018-09-04 23:24:14 -0400

    update

--- Lecture2/Lab2/geometry.js
+++ Lecture2/Lab2/geometry.js
@@ -14,20 +14,32 @@ module.exports = {
         errorChecking(length, "length");
         errorChecking(width, "width");
         errorChecking(height, "height");
+
+        const volume = length * width * height;
+        return volume;
     },
 
     surfaceAreaOfRectangularPrism: (length, width, height) => {
         errorChecking(length, "length");
         errorChecking(width, "width");
         errorChecking(height, "height");
+
+        const Area = 2*((width*length)+(height*length)+(height*width));
+        return Area;
     },
 
     volumeOfSphere: (radius) => {
         errorChecking(radius, "radius");
+
+        const Volume = (4/3)*(Math.PI)(radius*radius*radius);
+        return Volume;
     },
 
     surfaceAreaOfSphere: (radius) => {
         errorChecking(radius, "radius");
+
+        const Area = 4*(Math.PI)*(radius*radius);
+        return Area;
     },
 
 }
