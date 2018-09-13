56dc0f0e2134924ec0ba257d5e315e2d57442cc7
Thefish1996 <rfishkin1313@gmail.com>
2018-09-12 23:02:53 -0400

    update

---
+++ Lecture3/Lab 3/textMetrics.js
@@ -0,0 +1,12 @@
+const createMetrics = (text) => {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+}
+
+//REMEMBER TO CHECK FOR ERRORS
+
+
+
+module.exports = {
+    createMetrics,
+}
