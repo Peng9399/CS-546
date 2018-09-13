8f8fa72b308b3e92ee04d7f2f460bd3b27f0c01a
Thefish1996 <rfishkin1313@gmail.com>
2018-09-12 23:14:03 -0400

    update

--- Lecture3/Lab 3/textMetrics.js
+++ Lecture3/Lab 3/textMetrics.js
@@ -1,7 +1,14 @@
 const createMetrics = (text) => {
     const object = {};
+    const words = [];
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    for(let i = 0; i< text.length; i++){
+        alphabet.forEach(value => {
+             
+        });
+    }
 }
 
 //REMEMBER TO CHECK FOR ERRORS
