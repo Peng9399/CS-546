8a575666496db6c851a0fa72ff1f1f81634c668e
Thefish1996 <rfishkin1313@gmail.com>
2018-09-13 14:06:28 -0400

    update

--- Lecture3/Lab 3/textMetrics.js
+++ Lecture3/Lab 3/textMetrics.js
@@ -1,25 +1,27 @@
 const createMetrics = (text) => {
     const objectMetrics = {};
-    const words = text.split(" ");
+    const words = [];
 
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const vowels = ['a', 'e', 'i', 'o', 'u'];
-    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's',
-    't', 'v', 'w', 'x', 'y', 'z'];
+    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's','t', 'v', 'w', 'x', 'y', 'z'];
 
     let letters = 0;
     let nonLetters = 0;
     let vowelsNumber = 0;
     let consenentsNumber = 0;
+    let wordString = '';
 
-    for(let i = 0; i< text.length; i++){
+    for(let i = 0; i<= text.length; i++){
         const textLetter = text.charAt(i).toLowerCase();
 
         if(alphabet.includes(textLetter)) {    //sees if its a letter
             letters = letters + 1;
+            wordString += textLetter;    //concatinates the string with the current letter
         } else {
             nonLetters = nonLetters + 1;     //if its not a letter it adds to non letter
+            words.push(wordString);         //pushes the real word to the array
+            wordString = '';                //if its not a word it just resets wordstring
         }
 
 
@@ -39,14 +41,27 @@ const createMetrics = (text) => {
 
     }
 
+    const totalWords = words.filter(value => {         //filters the words array for just words
+        return value !== '';
+    });
+
+    const uniqueWords = totalWords.filter((value, index) => {
+        return index === totalWords.indexOf(value);         //this checks the new filtered totalwords for the correct index of each word, if duplicate, it doesnt find it
+    });
+
+
+
     objectMetrics["totalLetters"] = letters;
-    objectMetrics["totalNonLetters"] = nonLetters;
+    objectMetrics["totalNonLetters"] = nonLetters - 1;
     objectMetrics["totalVowels"] = vowelsNumber;
     objectMetrics["totalConsonants"] = consenentsNumber;
+    objectMetrics["totalWords"] = totalWords.length;
+    objectMetrics["uniqueWords"] = uniqueWords.length;
 
 
 
-  //  console.log(words)
+    console.log(uniqueWords);
+    console.log(totalWords);
     return console.log(objectMetrics);
 
 }
