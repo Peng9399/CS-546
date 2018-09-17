c99d22112ea4398b53a03c2112a93f6cedb2c56e
Thefish1996 <rfishkin1313@gmail.com>
2018-09-17 15:44:09 -0400

    update

--- Lecture3/Lab 3/app.js
+++ Lecture3/Lab 3/app.js
@@ -2,6 +2,9 @@ const dataFile = require("./fileData");
 const metrics = require("./textMetrics");
 
 async function tester(file) {
+    if (typeof file !== "string" || !file) {
+        throw "Please enter a correct file path"
+    }
     const result = file.split('/').pop().split('.');
     const resultJson = result[0] + '.result.json';
 
@@ -21,6 +24,14 @@ async function tester(file) {
     }
 
 }
-tester('./chapter1.txt').catch(error => {
-    console.log(error);
+tester(4).catch(error => {
+    console.log('There was an error: ', error);
 })
+
+tester('./chapter2.txt').catch(error => {
+    console.log('There was an error: ', error);
+})
+
+// tester('./chapter3.txt').catch(error => {
+//     console.log(error);
+// })
