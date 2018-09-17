16723dd0aecfb606d7428c60b880330652234d3f
Thefish1996 <rfishkin1313@gmail.com>
2018-09-17 01:07:58 -0400

    update

--- Lecture3/Lab 3/app.js
+++ Lecture3/Lab 3/app.js
@@ -1,7 +1,7 @@
 const dataFile = require("./fileData");
 const metrics = require("./textMetrics");
 
-async function tester (file) {
+async function tester(file) {
     const result = file.split('/').pop().split('.');
     const resultJson = result[0] + '.result.json';
 
@@ -9,7 +9,7 @@ async function tester (file) {
         let fileRead = await dataFile.getFileAsJSON(resultJson)
         console.log(fileRead);
     } catch (error) {
-        if(error.code === "ENOENT") {
+        if (error.code === "ENOENT") {
             let stringContents = await dataFile.getFileAsString(file);
             let objectContents = await metrics.createMetrics(stringContents);
             let saveJSON = await dataFile.saveJSONToFile(resultJson, objectContents);
@@ -21,6 +21,6 @@ async function tester (file) {
     }
 
 }
- tester('./chapter1.txt').catch(error => {
-     console.log(error);
- })
+tester('./chapter1.txt').catch(error => {
+    console.log(error);
+})
