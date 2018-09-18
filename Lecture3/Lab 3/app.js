133b11950443d659d4fac1ed33e78e73016f5a4b
Thefish1996 <rfishkin1313@gmail.com>
2018-09-18 01:28:34 -0400

    update

--- Lecture3/Lab 3/app.js
+++ Lecture3/Lab 3/app.js
@@ -10,16 +10,16 @@ async function tester(file) {
     const resultJson = result[0] + '.result.json';
 
     try {
-        let fileRead = await dataFile.getFileAsJSON(resultJson)
+        let fileRead = await dataFile.getFileAsJSON(resultJson)             //tries to read the resultJSon file
         console.log(fileRead);
     } catch (error) {
-        if (error.code === "ENOENT") {
-            let stringContents = await dataFile.getFileAsString(file);
-            let objectContents = await metrics.createMetrics(stringContents);
+        if (error.code === "ENOENT") {                                           //error code EONT will come from getFilasJson and cause ENOENT error
+            let stringContents = await dataFile.getFileAsString(file);            //gets the file as string
+            let objectContents = await metrics.createMetrics(stringContents);     // creates metrics
 
-            await dataFile.saveJSONToFile(resultJson, objectContents);
+            await dataFile.saveJSONToFile(resultJson, objectContents);           //creates a new .result.json file with the contents of file
 
-            let parseFile = await dataFile.getFileAsJSON(resultJson);
+            let parseFile = await dataFile.getFileAsJSON(resultJson);               //reads the newly created file
             console.log(parseFile);
         } else {
             throw error;
