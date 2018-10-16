f5e749aad9a9ebe59b661074a8d121ea2669d200
Thefish1996 <rfishkin1313@gmail.com>
2018-10-15 22:59:41 -0400

    update

--- Lecture7/Lab 7/data/recipes.js
+++ Lecture7/Lab 7/data/recipes.js
@@ -2,6 +2,7 @@ const collections = require("../config/mongoCollections");
 const recipes = collections.recipes;
 const uuid = require("node-uuid");
 
+
 const addRecipe = async (title, ingredients, steps) => {
     if(!title || typeof title !== "string") throw "Title was either not provided or isnt a string";
     if(!ingredients || !ingredients.isArray()) throw "Ingredients was either not provided or isnt a type of array";
@@ -25,7 +26,55 @@ const addRecipe = async (title, ingredients, steps) => {
 } 
 
 
+const allRecipes = async => {
+    const recipeCollection = await recipes();
+    return await recipeCollection.find({}).toArray();
+}
+
+
+const getRecipe = async id => {
+    if(!id) throw "No id was provided";
+
+    const recipeCollection = await recipes();
+    const findRecipe = await recipeCollection.findOne({"_id" : id})
+
+    if (findRecipe === null) throw 'No task was found with that given id';
+
+    return findRecipe;
+
+}
+
+
+const updateRecipe = async id => {
+
+}
+
+const patchRecipe = async id => {
+
+}
+
+
+const deleteRecipe = async id => {
+    if(!id) throw "No id was provided";
+
+    const recipeCollection = await recipes();
+    const removeRecipe = await recipeCollection.removeOne({ _id: id });
+
+    if (removeRecipe.deletedCount === 0) {
+        throw `Could not delete post with id of ${id}`;
+      }
+}
 
 
 
-module.exports = {}
+
+
+
+module.exports = {
+    addRecipe,
+    allRecipes,
+    getRecipe,
+    updateRecipe,
+    patchRecipe,
+    deleteRecipe
+}
