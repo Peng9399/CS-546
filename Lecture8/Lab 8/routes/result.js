899f43b822dda62fdea691ec79758bc67efe3c02
Thefish1996 <rfishkin1313@gmail.com>
2018-10-24 18:36:30 -0400

    update

--- Lecture8/Lab 8/routes/result.js
+++ Lecture8/Lab 8/routes/result.js
@@ -4,23 +4,33 @@ const checker = require("../checker")
 
 router.post("/", async(req , res) => {
     let wordTest = req.body["text-to-test"];
-    const validPalindrome = checker(wordTest); 
-    if(validPalindrome) {
-        res.render("palindrome/results", {
-            title: "The Palindrome Results!" ,
-            class: "success",
-            textToTest: wordTest,
-            statusMessage: "This Phrase IS a Palindrome" 
-        })
+    if(wordTest) {
+        const validPalindrome = checker(wordTest); 
+        if(validPalindrome) {
+            res.render("palindrome/results", {
+                title: "The Palindrome Results!" ,
+                class: "success",
+                textToTest: wordTest,
+                statusMessage: "This Phrase IS a Palindrome" 
+            });
+        } else {
+            res.render("palindrome/results", {
+                title: "The Palindrome Results!" ,
+                class: "failure",
+                textToTest: wordTest,
+                statusMessage: "This Phrase is NOT a Palindrome" 
+        });       
+        }
     } else {
-        res.render("palindrome/results", {
+        res.status(400)
+        res.render("palindrome/error", {
             title: "The Palindrome Results!" ,
-            class: "failure",
-            textToTest: wordTest,
-            statusMessage: "This Phrase is NOT a Palindrome" 
-        })        
+            class: "error",
+            errorMessage: "ERROR: No phrase was provided, please go back and provide a valid phrase!"             
+        });
     }
-})
+});
+
 
 
 module.exports = router;
