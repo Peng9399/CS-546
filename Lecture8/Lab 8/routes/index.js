cf3f10a3347fa22de9f35e5a755612277f154361
Thefish1996 <rfishkin1313@gmail.com>
2018-10-24 13:29:47 -0400

    update

--- Lecture8/Lab 8/routes/index.js
+++ Lecture8/Lab 8/routes/index.js
@@ -2,7 +2,7 @@ const result = require("./result");
 
 
 const constructorMethod = app => {
-    app.use("/results", result);
+    app.use("/result", result);
     app.get("/", async (req, res) => {
         res.render("palindrome/index", {
             title: "The Best Palindrome Checker in the World!"
