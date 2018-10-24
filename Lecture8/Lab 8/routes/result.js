10b4543042cf809ecf2958f17fce93fc18a71751
Thefish1996 <rfishkin1313@gmail.com>
2018-10-24 16:53:03 -0400

    update

--- Lecture8/Lab 8/routes/result.js
+++ Lecture8/Lab 8/routes/result.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const checker = require("../checker")
 
 router.post("/", async(req , res) => {
-    let wordTest = req.body;
-    res.json(wordTest)
+    let wordTest = req.body["text-to-test"];
+    const validPalindrome = checker(wordTest); 
+    res.json({testing: validPalindrome})
 })
 
 
