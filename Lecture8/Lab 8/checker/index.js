2f325e03e7ae8a6292522da13c4f32d7b0a4f7d0
Thefish1996 <rfishkin1313@gmail.com>
2018-10-24 14:40:55 -0400

    update

--- Lecture8/Lab 8/checker/index.js
+++ Lecture8/Lab 8/checker/index.js
@@ -1,6 +1,6 @@
 
 const checker = phrase => {
-    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let reverseString = "";
     for(let i = phrase.length; i >= 0; i-- ){
         let letter = phrase.charAt(i).toLowerCase();
