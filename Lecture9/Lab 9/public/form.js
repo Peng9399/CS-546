0a26454dfbf2825541898ce330fc9f81ff4bd5ef
Thefish1996 <rfishkin1313@gmail.com>
2018-10-31 15:44:16 -0400

    update

--- Lecture9/Lab 9/public/form.js
+++ Lecture9/Lab 9/public/form.js
@@ -1,5 +1,5 @@
 const checker = phrase => {
-    if(!phrase) throw "No phrase was provided."
+    if(!phrase) throw "Error: No phrase was provided."
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let reverseString = "";
     let forwardString = "";
@@ -24,6 +24,7 @@ const checker = phrase => {
 document.addEventListener("submit", event => {
     event.preventDefault();         //stops the page from refreshing
     const phrase = document.getElementsByName("phrase")[0].value        //its a list so gets 0 list value
+    const container = document.getElementById("error_container")
     try {
         const boolean = checker(phrase);
         const x = document.createElement("li");
@@ -35,8 +36,9 @@ document.addEventListener("submit", event => {
         }   
         const t = document.createTextNode(phrase);
         x.appendChild(t);
-        document.getElementById("attempts").appendChild(x);       
+        document.getElementById("attempts").appendChild(x);
+        container.innerHTML = ""      
     } catch (error) {
-        
+        container.innerHTML = error
     }
 })
