e851ce2aa346e83f3b3f4f32aabb7b68138eb2d7
Thefish1996 <rfishkin1313@gmail.com>
2018-11-06 18:41:04 -0500

    update

--- Lecture10/Lab 10/app.js
+++ Lecture10/Lab 10/app.js
@@ -18,4 +18,5 @@ configRoutes(app);
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log("Your routes will be running on http://localhost:3000");
+  if (process && process.send) process.send({done: true}); // ADD THIS LINE
 });
