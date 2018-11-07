378ee0e17909028cd3b754aa2bc6dbd29f0b3053
Thefish1996 <rfishkin1313@gmail.com>
2018-11-07 02:17:41 -0500

    update

--- Lecture10/Lab 10/routes/index.js
+++ Lecture10/Lab 10/routes/index.js
@@ -4,20 +4,37 @@ const users = require("../users");
 
 const constructorMethod = app => {
     app.get("/", (req, res, next) => {
-        // if(req.cookies.AuthCookie){               //checks to see if there is a cookie with value token
-        //     console.log("There is a cookie")
-        // } else {
+        if(req.cookies.AuthCookie){               //checks to see if there is a cookie with value token
+            res.redirect("/private")
+         } else {
             res.render("authentication/static", {}); //if no cookie it will render this page
-    //    }
+        }
     });
 
-    app.get("/private", (req, res, next) => {
-        if(!req.cookies.AuthCookie) {
-            res.status(403).send("You are not logged in!"); 
-        } else {
-            res.render("authentication/static", {});
-            console.log(req.cookies.AuthCookie.session)
+    app.get("/private", async (req, res, next) => {
+        try {
+            let session = req.cookies.AuthCookie._id
+            const userElement = await users.find(element => {
+                return element.id === session;
+            })
+            if(userElement) {
+                res.render("authentication/private", {
+                    id: userElement.id,
+                    userName: userElement.username,
+                    firstName: userElement.first_name,
+                    lastName: userElement.last_name
+                });
+            } else {
+                res.status(403).send("You are not logged in!");  
+            }
+        } catch (error) {
+            res.status(403).send("You are not logged in!");  
         }
+    });
+
+    app.get("/logout", (req, res, next) => {
+        res.clearCookie("AuthCookie");
+        res.render("authentication/logout", {});
     });
 
     app.post("/login", async (req, res, next) => {
@@ -31,14 +48,12 @@ const constructorMethod = app => {
                 return element.username === userName;
             })
 
-            comparePassword = await bcrypt.compare(passWord, userElement.hashedPassword)   //compares the hashed password
+            comparePassword = await bcrypt.compare(passWord, userElement.hashed_password)   //compares the hashed password
 
             if(comparePassword === true) {
                 userElement._id = sessionId;
                 res.cookie("AuthCookie", {session: userElement._id}); //creates a new cookie with value of AuthCookie
-                next();
-                console.log(userElement);
-                console.log(req.cookies)                
+                res.redirect("/private");           
             } else {
                 res.redirect("/");
             }
