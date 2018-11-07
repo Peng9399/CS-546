fb12753be8726b04f1bd14982daffb785d720af5
Thefish1996 <rfishkin1313@gmail.com>
2018-11-07 00:55:36 -0500

    update

--- Lecture10/Lab 10/routes/index.js
+++ Lecture10/Lab 10/routes/index.js
@@ -1,6 +1,45 @@
+const uuid = require("node-uuid");
+const bcrypt = require("bcrypt");
+const users = require("../users");
+
 const constructorMethod = app => {
-    app.get("/", (req, res) => {
-        res.render("authentication/static", {});
+    app.get("/", (req, res, next) => {
+        // if(req.cookies.AuthCookie){               //checks to see if there is a cookie with value token
+        //     console.log("There is a cookie")
+        // } else {
+            res.render("authentication/static", {}); //if no cookie it will render this page
+    //    }
+    });
+
+    app.get("/private", (req, res, next) => {
+
+    });
+
+    app.post("/login", async (req, res, next) => {
+        const userName = req.body["username"];
+        const passWord = req.body["password"];
+        const sessionId = uuid.v4();
+        let comparePassword = false;
+
+        try {
+            const userElement = await users.find(element => {
+                return element.username === userName;
+            })
+
+            comparePassword = await bcrypt.compare(passWord, userElement.hashedPassword)   //compares the hashed password
+
+            if(comparePassword === true) {
+                userElement._id = sessionId;
+                res.cookie("AuthCookie", {session: userElement._id}); //creates a new cookie with value of AuthCookie
+                next();
+                console.log(userElement);
+                console.log(req.cookies)                
+            } else {
+                res.redirect("/");
+            }
+        } catch (error) {
+            res.redirect("/");
+        }
     });
 }
 
---
+++ Lecture10/Lab 10/users.js
@@ -0,0 +1,6 @@
+const users = [
+    { _id: "32142314", username: "masterdetective123", hashedPassword: "THE HASH", firstName: "Sherlock", lastName: "holmes" }, // etc, dont forget the other data 
+    { _id: "123123123123", username: "lemon", hashedPassword: "$2a$16$SsR2TGPD24nfBpyRlBzINeGU61AH0Yo/CbgfOlU1ajpjnPuiQaiDm", firstName: "Elizabeth", lastName: "Lemon" }, // etc, dont forget the other data  
+  ]
+
+module.exports = users;
