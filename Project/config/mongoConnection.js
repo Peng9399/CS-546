284797dc5097b9918b756a922480bdda0424bf08
Thefish1996 <rfishkin1313@gmail.com>
2018-11-14 19:02:05 -0500

    update

--- Project/config/mongoConnection.js
+++ Project/config/mongoConnection.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 const settings = {
   mongoConfig: {
     serverUrl: "mongodb://localhost:27017/",
-    database: "lab7-recipes"
+    database: "Users"
   }
 };
 const mongoConfig = settings.mongoConfig;
