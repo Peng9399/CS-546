fc3a505ee9ea0575509237372c29582033e88203
Thefish1996 <rfishkin1313@gmail.com>
2018-11-14 19:21:20 -0500

    update

--- Project/data/users.js
+++ Project/data/users.js
@@ -1,4 +1,35 @@
 const collections = require("../config/mongoCollections")
 const userData = collections.Users
+const uuid = require("node-uuid");
 
-const addUser = async ()
+
+const addUser = async (userName) => {
+    if(!userId) throw "No user id"
+
+    let userObject = {
+        _id: uuid(),
+        UserID: userName
+    }
+
+    const userCollection = await userData()
+    const addUser = await userCollection.insertOne(userObject)
+    if (addUser.insertedCount === 0) throw "Could not create the recipe";
+
+}
+
+const getUser = async (id)  => {
+    if(!id) throw "No id was provided"
+    const userCollection = await userData()
+    const findUser = await userCollection.findOne({_id : id})
+
+    if (findUser === null) throw 'No recipe was found with that given id';
+
+    return findUser
+
+    
+}
+
+module.exports = {
+    addUser,
+    getUser
+}
